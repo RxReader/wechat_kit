e6fd5405094f076c57273d4103ba3ff247786b02
v7lin <v7lin@qq.com>
2023-06-02 14:52:54 +0800

    flutter upgrade

---
+++ android/src/test/java/io/github/v7lin/wechat_kit/WechatKitPluginTest.java
@@ -0,0 +1,29 @@
+package io.github.v7lin.wechat_kit;
+
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.verify;
+//
+//import io.flutter.plugin.common.MethodCall;
+//import io.flutter.plugin.common.MethodChannel;
+import org.junit.Test;
+
+/**
+ * This demonstrates a simple unit test of the Java portion of this plugin's implementation.
+ *
+ * Once you have built the plugin's example app, you can run these tests from the command
+ * line by running `./gradlew testDebugUnitTest` in the `example/android/` directory, or
+ * you can run them directly from IDEs that support JUnit such as Android Studio.
+ */
+
+public class WechatKitPluginTest {
+  @Test
+  public void onMethodCall_isInstalled_returnsExpectedValue() {
+//    WechatKitPlugin plugin = new WechatKitPlugin();
+//
+//    final MethodCall call = new MethodCall("isInstalled", null);
+//    MethodChannel.Result mockResult = mock(MethodChannel.Result.class);
+//    plugin.onMethodCall(call, mockResult);
+//
+//    verify(mockResult).success(true);
+  }
+}
