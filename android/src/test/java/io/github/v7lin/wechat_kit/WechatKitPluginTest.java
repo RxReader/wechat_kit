b4264b37a076569f9c0c5ce6447a25106db7377c
v7lin <v7lin@qq.com>
2023-06-02 21:59:17 +0800

    flutter upgrade

--- android/src/test/java/io/github/v7lin/wechat_kit/WechatKitPluginTest.java
+++ android/src/test/java/io/github/v7lin/wechat_kit/WechatKitPluginTest.java
@@ -1,10 +1,10 @@
 package io.github.v7lin.wechat_kit;
 
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.verify;
-//
-//import io.flutter.plugin.common.MethodCall;
-//import io.flutter.plugin.common.MethodChannel;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import org.junit.Test;
 
 /**
@@ -18,12 +18,12 @@ import org.junit.Test;
 public class WechatKitPluginTest {
   @Test
   public void onMethodCall_isInstalled_returnsExpectedValue() {
-//    WechatKitPlugin plugin = new WechatKitPlugin();
-//
-//    final MethodCall call = new MethodCall("isInstalled", null);
-//    MethodChannel.Result mockResult = mock(MethodChannel.Result.class);
-//    plugin.onMethodCall(call, mockResult);
-//
-//    verify(mockResult).success(true);
+    WechatKitPlugin plugin = new WechatKitPlugin();
+
+    final MethodCall call = new MethodCall("isInstalled", null);
+    MethodChannel.Result mockResult = mock(MethodChannel.Result.class);
+    plugin.onMethodCall(call, mockResult);
+
+    verify(mockResult).success(false);
   }
 }
